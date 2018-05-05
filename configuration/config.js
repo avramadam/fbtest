dbfa96b166a5e6cf872f5c2c8e525d7e125e4498
unknown <adam9312@comcast.net>
2018-05-05 07:54:04 -0700

    fbtest

--- configuration/config.js
+++ configuration/config.js
@@ -6,7 +6,7 @@ module.exports={
 
 	"facebook_api_key" 		: 			"1889441681121377",
 	"facebook_api_secret"	:			"9a20174c1ea7c13e8ca5a9992046f4fa",
-	"callback_url"			:			"https://localhost:3000/auth/facebook/callback",
+	"callback_url"			:			"http://localhost:3000/auth/facebook/callback",
 	"use_database"			:			"false",
 	"host"					:			"localhost",
 	"username"				:			"root",
